eed6ee4da5830436b875452a90bef6d9ba8a82a5
jaeyumn <wkddkgodsla@gmail.com>
2025-03-31 21:57:53 +0900

    init

---
+++ src/test/kotlin/noul/oe/OeApplicationTests.kt
@@ -0,0 +1,13 @@
+package noul.oe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
