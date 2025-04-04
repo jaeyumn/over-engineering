314247292713482ed545db2abcbb6939ced40b98
jaeyumn <wkddkgodsla@gmail.com>
2025-04-04 23:05:45 +0900

    chore: session 기반 security config 설정

--- src/main/kotlin/noul/oe/user/config/SecurityConfig.kt
+++ src/main/kotlin/noul/oe/user/config/SecurityConfig.kt
@@ -1,12 +1,38 @@
 package noul.oe.user.config
 
+import noul.oe.user.service.CustomUserDetailsService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val userDetailsService: CustomUserDetailsService
+) {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers("/api/users/sign-up", "/api/auth/log-in").permitAll()
+                it.anyRequest().authenticated()
+            }
+            .formLogin { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) }
+            .userDetailsService(userDetailsService)
+            .build()
+    }
+
+    @Bean
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager =
+        authenticationConfiguration.authenticationManager
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
